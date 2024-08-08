73b5b0aaa52025d3bcf096d526643000f411d010
a9na <anaiscoding@gmail.com>
2024-08-08 12:03:22 +0200

    commit

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -10,19 +10,33 @@ function ToDoList() {
     }
 
     function addTask(){
-        setTasks(prevTasks => [...prevTasks, newTask]);
+
+        if(newTask.trim() !== ""){
+            setTasks(prevTasks => [...prevTasks, newTask]);
+            setNewTask("");
+        }
     }
 
     function deleteTask(index){
-        
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
     }
 
     function moveTaskUp(index){
 
+        if (index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     function moveTaskDown(index){
-
+        if (index < tasks.length - 1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
 
